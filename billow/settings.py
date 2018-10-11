c15a3df671b278bc4849d3d221573a3142b9df0e
beamsplit <catherine.n.hughes@gmail.com>
2018-10-11 12:41:20 +0100

    updated settings for postgres_pw

--- billow/settings.py
+++ billow/settings.py
@@ -112,7 +112,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'postgres',
         'USER': 'postgres',
-        'PASSWORD': os.environ['POSTGRES_PW'],
+        'PASSWORD': os.getenv('POSTGRES_PW'),
         'HOST': 'localhost',
         'PORT': '5432',
 }
